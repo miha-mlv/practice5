f0e4d61df8da229273cddb984fd974f4e19c61a2
Миша <malorodovmihail009@gmail.com>
2024-03-16 23:36:47 +0300

    Init commit task2

--- app/src/main/java/com/example/practice5/MainActivity.java
+++ app/src/main/java/com/example/practice5/MainActivity.java
@@ -8,19 +8,9 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button main_btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_btn_start = findViewById(R.id.main_btn_start);
-        main_btn_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Activity2.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
